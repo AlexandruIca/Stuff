7e4ac22a015663ad2b6651949e7e711a2f1ea616
Alexandru-Gabriel Ică <alexandruica703@gmail.com>
2021-07-17 14:06:25 +0300

    Nix shell that considers bashrc

---
+++ scripts/shell.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {}; {
+    pythonEnv = stdenvNoCC.mkDerivation {
+        name = "python-environment";
+        buildInputs = with pkgs; [
+            python39
+        ];
+        shellHook = ''source ~/.bashrc'';
+    };
+}
